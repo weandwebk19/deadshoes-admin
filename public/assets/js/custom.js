6b5523f28a45eef7b1edd13276f30aae0c8f15f8
unknown <manhnguyen015.mnd@gmail.com>
2021-11-11 22:37:55 +0700

    Web admin tinh

---
+++ public/assets/js/custom.js
@@ -0,0 +1,12 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+const btnAdd = $('.btn-add');
+const formAdd = $('.needs-validation');
+
+let isHidden = true;
+
+btnAdd.onclick = function() {
+    isHidden = !isHidden;
+    formAdd.classList.toggle('hidden', isHidden);
+}
